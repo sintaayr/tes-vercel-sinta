34ef0a29be7393ee64cb76cfffcca7671068f661
sintaayr <21082010069@studentupnjatim.ac.id>
2023-07-23 02:28:18 +0700

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        Hai, aku adalah mahasiswa Sistem Informasi'21 UPN "Veteran" Jawa Timur. Selama perkuliahan, aku telah mempelajari banyak hal. 
+        Salah satu mata kuliah yang menurutku enjoy banget yaitu pemograman desktop (soalnya bisa copas code dari modul) :D 
+        Akhir-akhir ini untuk menambah produktivitas aku mengikuti beberapa workshop yang diadakan oleh HIMASIFO. Makasih kakak-kakak panitianya.
+        And big thanks for kak fajar, udah mau jadi mentor di workshop front-end with react.js selama 4 hari. See u all ^^ 
+        dan selanjutnya ini ada peluang kerja untuk kita dan teknologi yang digunakan.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
